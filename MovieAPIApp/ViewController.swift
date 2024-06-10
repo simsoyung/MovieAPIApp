5608385a22dcaebd0a9725ed33dad15f7fe12f7f
soyung sim <simsy1024@gmail.com>
2024-06-10 18:15:44 +0900

    Initial Commit

---
+++ MovieAPIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPIApp
+//
+//  Created by 심소영 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
